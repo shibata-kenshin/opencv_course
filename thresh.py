676b97ed8300270b558bc419bafe5ba3afcaa16e
shibata-kenshin <shibata@enaspiration.com>
2021-11-16 15:41:51 +0900

    add thresh

---
+++ thresh.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+img = cv.imread('Resources/Photos/cats.jpg')
+cv.imshow('Cats', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# Simple Threshholding
+threshold, thresh = cv.threshold(gray, 255, 255, cv.THRESH_BINARY )
+cv.imshow('Simple Thresholded', thresh)
+
+threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV )
+cv.imshow('Simple Thresholded', thresh_inv)
+
+# Adaptive Thresholding
+adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 13, 3)
+cv.imshow('Adaptive Thresh', adaptive_thresh)
+
+cv.waitKey(0)
